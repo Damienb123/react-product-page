16a2055135e83fc94344d98403ee40d393328e46
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2026-05-02 17:37:13 -0500

    Wrap App component with BrowserRouter and CheckoutProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './layouts/App.jsx'
+import { CheckoutProvider } from './components/CheckoutContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <CheckoutProvider>
+        <App />
+      </CheckoutProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
