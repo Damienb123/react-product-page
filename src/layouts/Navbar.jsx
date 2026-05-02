b891ce4d4e2005f7839c3d520a181f75771a8e5b
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2026-05-02 17:35:58 -0500

    Refactor Navbar component for improved structure

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,35 +1,36 @@
-import { UseState } from 'react'
-{/* Uses 3 icons from imported from react-icons */}
-import { IoPersonSharp, IoSearch, IoBagOutline } from "react-icons/io5";
-
-{/* global css */}
-import '../App.css'
-import { Link } from 'react-router-dom'
+import { IoBagOutline, IoPersonSharp, IoSearch } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { useCheckout } from "../components/useCheckout.js";
 
 function Navbar() {
-    return (
-        <>
-        {/* Header layout focused on links throughout the product page */}
-        <div className="parent-container bg-slate-50 flex justify-between items center">
-            <h3 className="navbar-title">
-                The Gallery
-            </h3>
-            <div className="nav-links">
-             <a href="#featured-artwork">Featured Artwork</a>
-             <a href="#shipping-process">Shipping</a>
-             <a href="#customer-reviews">Reviews</a>
+  const { itemCount } = useCheckout();
 
-            </div>
+  return (
+    <header className="parent-container">
+      <Link className="navbar-title" to="/">
+        The Gallery
+      </Link>
+      <nav className="nav-links" aria-label="Main navigation">
+        <Link to="/#featured-artwork">Featured Artwork</Link>
+        <Link to="/#shipping-process">Shipping</Link>
+        <Link to="/#customer-reviews">Reviews</Link>
+      </nav>
 
-            <div className="nav-images">
-             <IoSearch />
-             <IoPersonSharp />
-             <IoBagOutline />
-            </div>
-
-        </div>
-        </>
-    )
+      <div className="nav-images">
+        <button type="button" aria-label="Search">
+          <IoSearch />
+        </button>
+        <button type="button" aria-label="Account">
+          <IoPersonSharp />
+        </button>
+        <Link className="cart-nav-link" to="/checkout" aria-label="Checkout">
+          <IoBagOutline />
+          {itemCount > 0 && <span>{itemCount}</span>}
+        </Link>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
